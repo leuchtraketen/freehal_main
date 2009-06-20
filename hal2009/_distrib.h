4b1ac32f53bbf719cb8bf04f6a3b7f06cd0f4f9c
Tobias Schulz <tobiasschulz.dev@gmail.com>
2009-06-20 19:11:40 +0000

    + added freehal 2009 distribution service
    git-svn-id: http://freehal.googlecode.com/svn@22 22999e98-2dd5-6803-48d1-09e481d671f2

--- hal2009/_distrib.h
+++ hal2009/_distrib.h
@@ -19,6 +19,9 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
+#define USE_CXX 1
+#define NOT_HEADER_ONLY 1
+#define DONT_DECLARE_STD 1
 #include "hal2009.h"
 
 
