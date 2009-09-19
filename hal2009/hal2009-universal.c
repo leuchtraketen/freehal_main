6839b349bd433f9c1ce7cdd661a2e77b35e951a7
Tobias Schulz <tobiasschulz.dev@gmail.com>
2009-09-19 13:35:42 +0000

    - bug fix
    git-svn-id: http://freehal.googlecode.com/svn@365 22999e98-2dd5-6803-48d1-09e481d671f2

--- hal2009/hal2009-universal.c
+++ hal2009/hal2009-universal.c
@@ -677,7 +677,7 @@ struct fact* filter_fact_by_rules(struct fact* fact, struct request* request) {
     fact = 
     
             fact_matches_verb                 (fact, request)
-         && ( fact_matches_subject_by_subject (fact, request) || fact_matches_object_by_subject (fact, request) )
+         && ( fact_matches_subject_by_subject (fact, request) || (strcmp(request->context, "q_what_weakly") && fact_matches_object_by_subject (fact, request)) )
          && ( fact_matches_object_by_object   (fact, request) || fact_matches_object_by_object  (fact, request) )
          && fact_matches_adverb_by_adverb     (fact, request)
          && fact_matches_anything_by_extra    (fact, request)
