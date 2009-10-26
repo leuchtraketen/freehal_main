0b794e5f60b40f1a2690b4414c5cd0bbbdfe76d5
Tobias Schulz <tobiasschulz.dev@gmail.com>
2009-10-26 20:41:04 +0000

    - new wiki feature
    git-svn-id: http://freehal.googlecode.com/svn@563 22999e98-2dd5-6803-48d1-09e481d671f2

--- hal2009/hal2009-universal.c
+++ hal2009/hal2009-universal.c
@@ -988,8 +988,14 @@ struct fact** search_facts_simple(const char* subjects, const char* objects, con
         for (l = 0; can_be_a_pointer(list[l]) || -1 == list[l]; ++l) {
             if (can_be_a_pointer(list[l])) {
                 if (can_be_a_pointer(list[l]->verbs) && can_be_a_pointer(list[l]->verbs[0]) && can_be_a_pointer(list[l]->verbs[0]->name)) {
-                    if (strstr(list[l]->verbs[0]->name, "=") && strstr(list[l]->from, "thes")) {
-                        set_to_invalid_value(&(list[l]));
+                    if (strstr(list[l]->verbs[0]->name, "=")) {
+                        if (strstr(list[l]->from, "thes")) {
+                            set_to_invalid_value(&(list[l]));
+                        }
+                        else {
+                            free(list[l]->verbs);
+                            list[l]->verbs = divide_words("equal");
+                        }
                     }
                 }
             }
