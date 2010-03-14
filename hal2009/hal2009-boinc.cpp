80ab4b3f840510053ad9573e0f355870ca004eba
Tobias Schulz <tobiasschulz.dev@gmail.com>
2010-03-14 10:25:38 +0000

    - bug fix
    git-svn-id: http://freehal.googlecode.com/svn@960 22999e98-2dd5-6803-48d1-09e481d671f2

--- hal2009/hal2009-boinc.cpp
+++ hal2009/hal2009-boinc.cpp
@@ -1,3 +1,24 @@
+/*
+ * This file is part of FreeHAL 2010.
+ *
+ * Copyright(c) 2006, 2007, 2008, 2009, 2010 Tobias Schulz and contributors.
+ * http://freehal.org
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
 #define NOT_HEADER_ONLY 1
 #define USE_CXX 1
 #include "hal2009.h"
@@ -113,7 +134,7 @@ int main (int argc, char** argv) {
     options.handle_process_control = false;
     options.send_status_msgs = true;
     boinc_init_options(&options);
-    fprintf(stderr, "freehal 2009: starting...\n");
+    fprintf(stderr, "freeHAL 2010: starting...\n");
     
     string resolved_name;
     boinc_resolve_filename_s("boincXYZ.pro", resolved_name);
--- hal2009/hal2009.c
+++ hal2009/hal2009.c
@@ -1,12 +1,12 @@
 /*
- * This file is part of halfreeHAL 2009.
+ * This file is part of FreeHAL 2010.
  *
- * Copyright(c) 2006, 2007, 2008, 2009 Tobias Schulz and contributors.
+ * Copyright(c) 2006, 2007, 2008, 2009, 2010 Tobias Schulz and contributors.
  * http://freehal.org
  * 
- * This program is halfree software; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
- * as published by the halfree Software Foundation; either version 3
+ * as published by the Free Software Foundation; either version 3
  * of the License, or any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -15,7 +15,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the halfree Software
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
