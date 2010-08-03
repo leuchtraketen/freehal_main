ac64bf1a59634e24446baa7d25831f0fbdb91d7f
Tobias Schulz <tobiasschulz.dev@gmail.com>
2010-08-03 12:35:10 +0000

    bug fix
    git-svn-id: http://freehal.googlecode.com/svn@1129 22999e98-2dd5-6803-48d1-09e481d671f2

--- hal2009/hal2009.c
+++ hal2009/hal2009.c
@@ -295,7 +295,7 @@ int remove_negation (char* line, double* truth_ref, int* only_logic) {
     sline.s = l;
     strncpy(l, line, 4196);
     
-    printf("\nline (1): %s\n", line);
+//    printf("\nline (1): %s\n", line);
     if (strstr(line, "kein")) {
         sline_ref = replace(sline_ref, "kein", "ein");
         (*truth_ref) = 0.0;
@@ -312,7 +312,7 @@ int remove_negation (char* line, double* truth_ref, int* only_logic) {
         sline_ref = replace(sline_ref, "nicht ", " ");
         (*truth_ref) = 0.0;
     }
-    printf("\nline (2): %s\n", line);
+//    printf("\nline (2): %s\n", line);
     /*if (strstr(line, "nicht") == line) {
         sline_ref = replace(sline_ref, "nicht", " ");
         (*truth_ref) = 0.0;
