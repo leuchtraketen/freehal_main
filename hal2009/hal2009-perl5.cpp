8b40a142ca15c25c8fa2979fe85f79487fb906e9
Tobias Schulz <tobiasschulz.dev@gmail.com>
2011-04-10 19:50:55 +0000

    update
    git-svn-id: http://freehal.googlecode.com/svn@1367 22999e98-2dd5-6803-48d1-09e481d671f2

--- hal2009/hal2009-perl5.cpp
+++ hal2009/hal2009-perl5.cpp
@@ -89,8 +89,12 @@ static inline void perl5_convert_structure (string& hals, int just_compile) {
     hals = replace(hals, " is true", " ");
     hals = replace(hals, " is wrong", " == 0 ");
     hals = replace(hals, " is false", " == 0 ");
-    hals = replace(hals, " true", " 1");
-    hals = replace(hals, " false", " 0");
+    if (string::npos == hals.find(" true_")) {
+        hals = replace(hals, " true", " 1");
+    }
+    if (string::npos == hals.find(" false_")) {
+        hals = replace(hals, " false", " 0");
+    }
     hals = replace(hals, " the hash ", " from hash ");
     hals = replace(hals, " has element ", " element ");
     hals = replace(hals, " contains element ", " element ");
